296dee6065d3a21b0d5747b1f4af359fb8b7f31b
LuisCode925 <195925luis@gmail.com>
2021-11-08 20:11:51 -0600

    Pagina Final

--- includes/hero.php
+++ includes/hero.php
@@ -4,8 +4,8 @@
             <a href="index.php"><b>One</b>-way</a>
             <nav class="navegacion">
                 <a href="registro.php">Registro</a>
-                <a href="aviso.php">Aviso</a>
-                <a href="about.php">Nosotros</a>
+                <a href="aviso.php">Aviso Legal</a>
+                <a href="about.php">Acerca de Nosotros</a>
             </nav>
         </div> <!-- Barra de enlaces -->
 
